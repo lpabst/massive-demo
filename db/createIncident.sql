214b84c9b892b8ff11217181ca32793b6c08771e
loren pabst <loren pabst>
2017-05-17 12:36:15 -0600

    post api now returns all incidents

--- db/createIncident.sql
+++ db/createIncident.sql
@@ -1,8 +1,3 @@
 
 insert into incidents (state, injuryId, causeId) 
 values ($1, $2, $3) 
-returning incidents.id as incidentId, incidents.state, injuries.name as injuryName, affectedareas.name as affectedArea, causes.name as cause from incidents
-join injuries on injuries.id = incidents.injuryId
-join affectedAreas on affectedAreas.id = injuries.affectedareaid
-join causes on causes.id = incidents.causeid
-order by incidentid
